6c383e1525cb2b2f0865c79c6e8eb76c9e4f7b29
teeai <57489008+teeai@users.noreply.github.com>
2019-12-11 17:35:42 +0800

    Update Staff.java

--- src/pastyear16n17q2/Staff.java
+++ src/pastyear16n17q2/Staff.java
@@ -13,7 +13,7 @@ public class Staff extends Income{
     //data member
     private String name;
     private String id;
-    private Income income;
+    private Income incomes;
     private double totalnexincome;
     
     //constructor
